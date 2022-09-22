b20ff3d581e9e6efec9a0bb861201f711ffe2f4f
sayedhany <sayed591999@gmail.com>
2022-09-23 01:51:31 +0200

    some theory lectures

--- script.js
+++ script.js
@@ -2,10 +2,15 @@
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforebegin', msg);
 
+  // countriesContainer.style.fontSize = '30px';
+};
 ///////////////////////////////////////
 
 function renderCountry(data, className = '') {
+  console.log(data);
   const html = `
     <article class="country ${className}">
           <img class="country__img" src="${data.flags.png}" />
@@ -25,7 +30,6 @@ function renderCountry(data, className = '') {
         </article>
     `;
   countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
 }
 // const getCountryAndNeighbour = function (country) {
 //   const request = new XMLHttpRequest();
@@ -63,11 +67,48 @@ function renderCountry(data, className = '') {
 //       renderCountry(data[0]);
 //     });
 // };
+
 const request = fetch(`https://restcountries.com/v3.1/name/egypt`);
 console.log(request);
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => {
+      if (!response.ok) throw new Error(`Country not found ${response.status}`);
+      return response.json();
+    })
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[1];
+      if (!neighbour) return;
+      console.log(neighbour);
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => renderCountry(data[0], 'neighbour'))
+    .catch(err => {
+      console.error(err);
+      renderError(`Something went Error ${err.message}`);
+    })
+    .finally(function () {
+      countriesContainer.style.opacity = 1;
+    });
 };
-getCountryData('egypt');
+btn.addEventListener('click', function () {
+  getCountryData('egypt');
+});
+
+// function whereAmI(lat, lng) {
+//   if (!lat || !lng) throw new Error('Enter truly values');
+//   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+//     .then(response => {
+//       // console.log(response);
+//       return response.json();
+//     })
+//     .then(data => console.log(data));
+// }
+
+// whereAmI(52.588, 13.381);
+console.log(`Test start`);
+setTimeout(() => console.log(`0 sec timer`), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+console.log('Test end');
